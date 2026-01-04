58c4ed65b8daacc748b66525a896b1322bba736b
Datom-code <enochgbadebo007@gmail.com>
2026-01-04 14:05:16 +0100

    renamed and configured the entire project to "clix"

---
+++ clix/clix_app/views.py
@@ -0,0 +1,23 @@
+from django.shortcuts import render, redirect, get_object_or_404
+from .models import ClixShortner
+
+# Create your views here.
+def home(request):
+    # new_url = None
+    new_url = request.session.pop('new_url', None)
+
+    if request.method =="POST":
+        
+        original_url = request.POST.get('original_url')
+        obj = ClixShortner.objects.create(original_url=original_url)
+        new_url = request.build_absolute_uri('/') + obj.shortened_link
+
+        request.session['new_url'] = new_url
+        
+        return redirect('home')
+    return render(request, 'index.html', {'new_url': new_url})
+    
+def redirect_link(request, shortened_link):
+    link = get_object_or_404(ClixShortner, shortened_link= shortened_link)
+
+    return redirect(link.original_url)
